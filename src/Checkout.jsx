3b55a5293eb1e6813d90c9456e8b7cde44df18dd
Parthvaghle123 <vaghelaparth2005@gmail.com>
2026-01-10 10:30:54 +0530

    anychange

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { QRCodeSVG } from "qrcode.react";
-import "./css/cart.css";
+import "./css/Cart.css";
 
 const Check = () => {
   const navigate = useNavigate();
